32a9f741feec88a21ad8302e9cbb4d1069ff02bd
Youssef M <youssef.mouaou@gmail.com>
2021-03-14 23:53:08 +0100

    affichage d'image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
         this.categories=data;
         console.log("u here");
       },err=>{
-        console.log("Error");
+        console.log(err);
       }
     )
   }
